11d179386122c294c4cb2ad94fc0992525bdb3c0
vinay umang <vinayumang6@gmail.com>
2025-12-18 09:09:04 +0530

    Second Commit

---
+++ Scikit-Learn/1.0 Machine Learning Intro.py
@@ -0,0 +1,27 @@
+"""
+ Machine Learning is a subset of artificial intelligence that 
+ focuses on building systems that can learn from and make decisions based on data. 
+ It involves the use of algorithms and statistical models to enable computers to improve 
+ their performance on a specific task over time without being explicitly programmed.
+   
+     Scikit-Learn is a popular open-source machine learning library for Python. It provides simple and efficient tools for data mining and data analysis, making it accessible to both beginners and experienced practitioners. Scikit-Learn supports various supervised and unsupervised learning algorithms, including classification, regression, clustering, and dimensionality reduction.
+main uses of machine learning:
+
+1. Predictive Analytics: Using historical data to make predictions about future events. 
+2. Image and Speech Recognition: Identifying objects in images or transcribing spoken words into text. 
+3. Natural Language Processing (NLP): Analyzing and understanding human language.
+
+Types of Machine Learning:
+1. Supervised Learning: The model is trained on labeled data, meaning that each training example is paired with an output label.
+Examples prediction of house prices based on features like size, location, and number of bedrooms.
+Algorithms: Linear Regression, Decision Trees, Support Vector Machines (SVM), Neural Networks,Random Forests.
+
+2. Unsupervised Learning: The model is trained on unlabeled data and must find patterns and relationships within the data on its own.
+Examples customer segmentation based on purchasing behavior without predefined categories.
+Algorithms: K-Means Clustering, Hierarchical Clustering, Principal Component Analysis (PCA), Autoencoders.
+
+3. Reinforcement Learning: The model learns by interacting with an environment and receiving feedback in the form of rewards or penalties.  
+Examples training an agent to play a game by rewarding it for achieving high scores.
+Algorithms: Q-Learning, Deep Q-Networks (DQN), Policy Gradient Methods.
+
+"""
