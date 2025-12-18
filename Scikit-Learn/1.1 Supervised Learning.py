9bbdb37fafd998385d4a066b20423f42f535e2e2
vinay umang <vinayumang6@gmail.com>
2025-12-18 09:22:57 +0530

    First Commit

---
+++ Scikit-Learn/1.1 Supervised Learning.py
@@ -0,0 +1,15 @@
+#Techniques for Supervised Learning in Scikit-Learn
+"""
+Types of Regression Algorithms:
+Linear Regression, Logistic Regression, Decision Trees, Random Forests,
+Support Vector Machines (SVM), K-Nearest Neighbors (KNN), Naive Bayes, 
+and Neural Networks (including CNNs for image analysis). 
+"""
+
+#Scikit-Learn is a library used for machine learning in Python.
+#commonly used functions in scikit learn are train_test_split, fit, predict, score
+
+#Importing necessary libraries
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error  
