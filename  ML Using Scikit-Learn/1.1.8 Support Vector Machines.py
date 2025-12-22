470306a7e800a7932bac639626e266cc7f6474e9
vinay umang <vinayumang6@gmail.com>
2025-12-22 23:06:04 +0530

    Renaming

---
+++  ML Using Scikit-Learn/1.1.8 Support Vector Machines.py
@@ -0,0 +1,18 @@
+"""Introduction to Support Vector Machines (SVM) using Scikit-Learn.
+ SVM is a powerful supervised machine learning algorithm used for classification and regression tasks.
+ It works by finding the optimal hyperplane that separates data points of different classes in a high-dimensional space.
+ 
+ SVM identifies the hyperplane that maximizes the distance to the nearest data points from each class, 
+ known as the hard margin approach for linearly separable data. For non-linear data, kernel functions transform the 
+ input space into a higher-dimensional one where separation becomes feasible
+ Uses: 
+- Text classification
+- Image classification
+- Bioinformatics (e.g., gene classification)
+- Handwriting recognition
+- Face detection
+
+SVM Types:
+- Linear SVM: Used for linearly separable data.
+- Non-linear SVM: Uses kernel functions (like RBF, polynomial) to handle non-linear data.
+"""
