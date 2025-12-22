aabb9c1bacea2b7ff97cb3a2187562905efdfc14
vinay umang <vinayumang6@gmail.com>
2025-12-22 23:21:40 +0530

    First Commit

---  ML Using Scikit-Learn/1.1.8 Support Vector Machines.py
+++  ML Using Scikit-Learn/1.1.8 Support Vector Machines.py
@@ -16,3 +16,30 @@ SVM Types:
 - Linear SVM: Used for linearly separable data.
 - Non-linear SVM: Uses kernel functions (like RBF, polynomial) to handle non-linear data.
 """
+import matplotlib.pyplot as plt
+import pandas as pd
+from sklearn import svm
+# x = [[2,3],[3,4],[1,1],[5,6],[6,7],[4,3],[6,8]]
+# y = [0,0,0,1,1,0,1]
+df=pd.read_csv("Excel/SVM-LinearKernalFunction.csv")
+x=df[['X','Y']].values
+y=df['Class'].values
+model = svm.SVC(kernel='linear')
+model.fit(x,y)
+x1 = int(input("Enter x cordinate : "))
+y1 = int(input("Enter the y coordinate : "))
+feature =[]
+feature.append(x1)
+feature.append(y1)
+pred = model.predict([feature])
+print("class purpose by model : ",pred[0])
+for i, point in enumerate(x):
+    if y[i] == 0:
+        plt.scatter(point[0], point[1],marker ='o')
+    else:
+        plt.scatter(point[0],point[1], marker='s')
+
+plt.title("Linear Scatter ")
+plt.xlabel("x")
+plt.ylabel('class')
+plt.show()
