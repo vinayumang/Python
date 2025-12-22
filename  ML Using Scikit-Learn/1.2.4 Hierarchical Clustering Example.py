eca6c35e686cc896b7992d14ff5c13acbe722edd
vinay umang <vinayumang6@gmail.com>
2025-12-23 01:09:53 +0530

    First commit

---
+++  ML Using Scikit-Learn/1.2.4 Hierarchical Clustering Example.py
@@ -0,0 +1,19 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt 
+from scipy.cluster.hierarchy import dendrogram, linkage
+from sklearn.cluster import AgglomerativeClustering
+
+df = pd.read_csv("Excel/HierarchicalClustering.csv")
+print(df)
+df1 = df[['Income','Expense']]
+X = np.array(df1)
+plt.figure(figsize=(8,5))
+linked = linkage(X, method ='ward')
+
+dendrogram = dendrogram(linked,orientation= 'top', show_leaf_counts =True)
+
+plt.xlabel("Data Mearge Point")
+plt.ylabel("Difference Income / Expense")
+plt.title("Organisation Income/ Expense Dendrogram")
+plt.show()
